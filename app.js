57822fe402a438ed0bedd75ae6355a901d6b5c69
Zygimantas Magelinskas <zygimantelis@gmail.com>
2017-03-24 14:57:13 +0200

    improved osu stats

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const Discord = require('discord.js');
 const _ = require("underscore");
 
 const settings = require('./settings.json');
-const commands = require('./commands.js')
+const commands = require('./commands.js');
 
 const client = new Discord.Client();
 
-const delimiter = "."
-const commandList = {".ping": commands.ping, ".osu": commands.osustats, ".bio": commands.bio, ".bot": commands.bot}
+const delimiter = ".";
+const commandList = {".ping": commands.ping, ".osu": commands.osustats, ".bio": commands.bio, ".bot": commands.bot};
 
 client.on('ready', () => {
   console.log('I am ready!');
@@ -21,6 +21,12 @@ client.on('message', message => {
 
       if (_.has(commandList, parsedCommand)) {
           commandList[parsedCommand](message);
+      } else {
+          console.log("asdasd");
+          embed = new Discord.RichEmbed();
+          embed.setDescription(message.content);
+          embed.setColor(0xFe3030)
+          message.edit({embed: embed});
       }
   }
 });
